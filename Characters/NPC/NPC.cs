787bec50bca65a0ca67e1cb5f4c2d7c592c4230f
Erethyon <erethyon@gmail.com>
2025-02-15 12:04:15 +0300

    No more WeaponNode field

--- Characters/NPC/NPC.cs
+++ Characters/NPC/NPC.cs
@@ -4,14 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using static StaticExtensions;
 
+
 /// <summary>
 /// Класс НИПов со способностью ориентироваться в пространстве
 /// </summary>
 public abstract partial class NPC : Entity
 {
-	// Общие характеристики
-	protected AnimatedWeapon? weaponNode;
-
 	// Ноды логики поведения
 	[Export] protected Node2D? targetNode;
 	protected Vector2 direction = Vector2.Zero;			// для расчёта направления движения
@@ -23,16 +21,12 @@ public abstract partial class NPC : Entity
 
 
 	public Node2D TargetNode {get => targetNode; set => targetNode = value;}
-	public AnimatedWeapon? WeaponNode => weaponNode;
 	public Vector2 Direction => Velocity.Normalized();
 	public float PatrolSpeedMultiplier => patrolSpeedMultiplier;
 	public float MoveSpeed => moveSpeed;
 
   	public override void _Ready()
   	{
-		base._Ready();		
-		try { weaponNode = GetChildren().OfType<AnimatedWeapon>().First(); }
-		catch(Exception) {GD.PushWarning($"weaponNode not found in {Name} Scene Tree");}
-		weaponNode.SetFacingDirection(direction);
+		base._Ready();
   	}
 }
